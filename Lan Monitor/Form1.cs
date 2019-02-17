8c4800498d2b7be8f46a7b8d1c7c7d62d55dc028
Artur Foden <FODENA@tcd.ie>
2019-02-17 00:28:16 +0000

    v0.1

--- Lan Monitor/Form1.cs
+++ Lan Monitor/Form1.cs
@@ -4,9 +4,14 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+using System.Threading;
+using System.Diagnostics;
 
 namespace Lan_Monitor {
     public partial class Form1 : Form {
@@ -15,6 +20,25 @@ namespace Lan_Monitor {
         }
 
         private void button1_Click(object sender, EventArgs e) {
+            try {
+                String returnMessage = String.Empty;
+                IPAddress[] ipAddresses;
+
+                ipAddresses = Dns.GetHostAddresses(textBox5.Text);
+
+                foreach (IPAddress ip in ipAddresses) {
+                    returnMessage = returnMessage + " " + ip + " ";
+                }
+
+                textBox1.Text = returnMessage;
+            }
+            catch(Exception ex) {
+                Console.WriteLine(ex.ToString());
+                textBox1.Text = ex.ToString();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
 
         }
     }
