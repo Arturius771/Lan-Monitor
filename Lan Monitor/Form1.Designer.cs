879599215a26f70241958435a5aa4693d9fe0e9d
Artur Foden <FODENA@tcd.ie>
2019-02-17 12:52:44 +0000

    v0.2

--- Lan Monitor/Form1.Designer.cs
+++ Lan Monitor/Form1.Designer.cs
@@ -28,6 +28,8 @@
             this.textBox5 = new System.Windows.Forms.TextBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -35,11 +37,11 @@
             // 
             this.button1.Location = new System.Drawing.Point(12, 38);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(156, 20);
+            this.button1.Size = new System.Drawing.Size(156, 24);
             this.button1.TabIndex = 0;
             this.button1.Text = "Find";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // textBox5
             // 
@@ -47,6 +49,9 @@
             this.textBox5.Name = "textBox5";
             this.textBox5.Size = new System.Drawing.Size(156, 20);
             this.textBox5.TabIndex = 6;
+            this.textBox5.Text = "Enter Hostname";
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox5.Click += new System.EventHandler(this.TextBox5_Click);
             // 
             // textBox1
             // 
@@ -54,34 +59,60 @@
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(614, 46);
+            this.textBox1.Size = new System.Drawing.Size(615, 50);
             this.textBox1.TabIndex = 7;
             this.textBox1.Text = "IP Addresses";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
-            this.pictureBox1.Location = new System.Drawing.Point(688, 338);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 113);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(100, 100);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.Size = new System.Drawing.Size(156, 90);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 8;
             this.pictureBox1.TabStop = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(174, 83);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox3.Size = new System.Drawing.Size(615, 120);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.WordWrap = false;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 83);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(156, 24);
+            this.button2.TabIndex = 11;
+            this.button2.Text = "Save Report";
+            this.button2.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(799, 451);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox3);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
             this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
             this.Text = "LAN Monitor";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -96,6 +127,8 @@
         private System.Windows.Forms.TextBox textBox5;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Lan Monitor/Form1.cs
+++ Lan Monitor/Form1.cs
@@ -19,27 +19,50 @@ namespace Lan_Monitor {
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e) {
-            try {
-                String returnMessage = String.Empty;
-                IPAddress[] ipAddresses;
-
-                ipAddresses = Dns.GetHostAddresses(textBox5.Text);
-
-                foreach (IPAddress ip in ipAddresses) {
-                    returnMessage = returnMessage + " " + ip + " ";
-                }
-
-                textBox1.Text = returnMessage;
-            }
-            catch(Exception ex) {
-                Console.WriteLine(ex.ToString());
-                textBox1.Text = ex.ToString();
-            }
+        private void Button1_Click(object sender, EventArgs e) {
+            FindIP();
         }
 
         private void Form1_Load(object sender, EventArgs e) {
 
+        }
+
+        private void FindIP() {
+            String returnMessage = String.Empty;
+            IPAddress[] ipAddresses;
+
+            try {
+                ipAddresses = Dns.GetHostAddresses(textBox5.Text);
+
+                foreach (IPAddress ip in ipAddresses) {
+                    returnMessage = returnMessage + " " + ip + " ";
+                    PingIP(ip);
+                }
+
+                textBox1.Text = returnMessage;
+            }
+            catch (Exception ex) {
+                Console.WriteLine(ex.ToString());
+                textBox1.Text = ex.ToString();
+            }
+        }//takes the hostname, find the IP addresses, and then pings each one and adds it to the result box
+
+        private void PingIP(IPAddress ip) {
+            Ping ping = new Ping();
+            PingReply reply;
+
+            reply = ping.Send(ip);
+
+            if(reply.Status == IPStatus.Success) {
+                textBox3.Text = textBox3.Text  + "Ping to " + textBox5.Text + " " + ip.ToString() + "[" + reply.ToString() + "] " + "SUCCESSFUL - " + "Response delay = " + reply.RoundtripTime.ToString() + "ms" + "\r\n";
+            }
+            else {
+                textBox3.Text = textBox3.Text + "Ping to " + textBox5.Text + " " + ip.ToString() + "[" + reply.ToString() + "] " + "FAILED - " + "Response delay = " + reply.RoundtripTime.ToString() + "ms" + "\r\n";
+            }
+        }//takes the IP addresses from FindIP() and then creates a report on their pingability
+
+        private void TextBox5_Click(object sender, EventArgs e) {
+            textBox5.Text = "";
         }
     }
 }
