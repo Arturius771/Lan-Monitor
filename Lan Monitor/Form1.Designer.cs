7758990915d4737208e3622986d0d53f2022e495
Artur Foden <FODENA@tcd.ie>
2019-02-18 22:17:58 +0000

    v0.3 building gui, no functionality

--- Lan Monitor/Form1.Designer.cs
+++ Lan Monitor/Form1.Designer.cs
@@ -33,14 +33,24 @@
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button5 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 38);
+            this.button1.Location = new System.Drawing.Point(12, 90);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(156, 24);
+            this.button1.Size = new System.Drawing.Size(155, 25);
             this.button1.TabIndex = 0;
             this.button1.Text = "Find";
             this.button1.UseVisualStyleBackColor = true;
@@ -48,9 +58,9 @@
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(12, 12);
+            this.textBox5.Location = new System.Drawing.Point(13, 12);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(156, 20);
+            this.textBox5.Size = new System.Drawing.Size(155, 20);
             this.textBox5.TabIndex = 6;
             this.textBox5.Text = "Enter Hostname";
             this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
@@ -58,52 +68,53 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(174, 12);
+            this.textBox1.Location = new System.Drawing.Point(172, 12);
             this.textBox1.Multiline = true;
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(615, 50);
+            this.textBox1.Size = new System.Drawing.Size(750, 50);
             this.textBox1.TabIndex = 7;
             this.textBox1.Text = "IP Addresses";
             this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // pictureBox1
             // 
-            this.pictureBox1.BackColor = System.Drawing.SystemColors.Control;
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
-            this.pictureBox1.Location = new System.Drawing.Point(12, 128);
+            this.pictureBox1.Location = new System.Drawing.Point(12, 180);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(156, 137);
+            this.pictureBox1.Size = new System.Drawing.Size(155, 137);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
             this.pictureBox1.TabIndex = 8;
             this.pictureBox1.TabStop = false;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(174, 83);
+            this.textBox3.Location = new System.Drawing.Point(172, 68);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
             this.textBox3.ReadOnly = true;
             this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
-            this.textBox3.Size = new System.Drawing.Size(615, 120);
+            this.textBox3.Size = new System.Drawing.Size(750, 100);
             this.textBox3.TabIndex = 10;
             this.textBox3.WordWrap = false;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 98);
+            this.button2.Location = new System.Drawing.Point(12, 152);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(156, 24);
+            this.button2.Size = new System.Drawing.Size(155, 25);
             this.button2.TabIndex = 11;
             this.button2.Text = "Save Report";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(12, 68);
+            this.button3.Location = new System.Drawing.Point(12, 121);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(156, 24);
+            this.button3.Size = new System.Drawing.Size(155, 25);
             this.button3.TabIndex = 12;
             this.button3.Text = "Find and Auto Update";
             this.button3.UseVisualStyleBackColor = true;
@@ -117,12 +128,125 @@
             this.notifyIcon1.Text = "notifyIcon1";
             this.notifyIcon1.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
             // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(12, 38);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(155, 20);
+            this.textBox2.TabIndex = 13;
+            this.textBox2.Text = "Username";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(13, 64);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(155, 20);
+            this.textBox4.TabIndex = 14;
+            this.textBox4.Text = "Password";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(12, 323);
+            this.textBox6.Multiline = true;
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(155, 20);
+            this.textBox6.TabIndex = 15;
+            this.textBox6.Text = "Localhost Name";
+            this.textBox6.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(12, 349);
+            this.textBox7.Multiline = true;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox7.Size = new System.Drawing.Size(300, 200);
+            this.textBox7.TabIndex = 16;
+            this.textBox7.Text = "Drive Information";
+            this.textBox7.WordWrap = false;
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(622, 236);
+            this.textBox8.Multiline = true;
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox8.Size = new System.Drawing.Size(300, 313);
+            this.textBox8.TabIndex = 17;
+            this.textBox8.Text = "Drive Information";
+            this.textBox8.WordWrap = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 304);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(102, 13);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "This PC Information:";
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(622, 183);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.Size = new System.Drawing.Size(155, 20);
+            this.textBox9.TabIndex = 19;
+            this.textBox9.Text = "Enter IP";
+            this.textBox9.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(783, 209);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(140, 25);
+            this.button4.TabIndex = 20;
+            this.button4.Text = "Ping Test";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "30",
+            "60",
+            "300",
+            "Infinite"});
+            this.comboBox1.Location = new System.Drawing.Point(622, 209);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(155, 21);
+            this.comboBox1.TabIndex = 21;
+            this.comboBox1.Text = "How many attempts?";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(783, 183);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(140, 25);
+            this.button5.TabIndex = 22;
+            this.button5.Text = "Ping 8.8.8.8";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.ClientSize = new System.Drawing.Size(799, 451);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(934, 561);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.textBox3);
@@ -154,6 +278,16 @@
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button5;
     }
 }
 
--- Lan Monitor/Form1.cs
+++ Lan Monitor/Form1.cs
@@ -12,20 +12,42 @@ using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Threading;
 using System.Diagnostics;
+using System.IO;
 
 namespace Lan_Monitor {
     public partial class Form1 : Form {
+
+        string hostName = Dns.GetHostName();
+        DriveInfo[] allDrives = DriveInfo.GetDrives();
+
         public Form1() {
             InitializeComponent();
+            GetLocalInformation();
+        }//starts the form, and displays local machine information
+
+        private void GetLocalInformation() {
+            textBox6.Text = "Localhost: " + hostName;
+            textBox7.Text = String.Empty;
+            foreach (DriveInfo d in allDrives) {
+                textBox7.Text = textBox7.Text + "Drive " + d.Name + "\r\n";
+                textBox7.Text = textBox7.Text + "  Drive type: " + d.DriveType + "\r\n";
+                if (d.IsReady == true) {
+                    textBox7.Text = textBox7.Text + "  Volume label: " + d.VolumeLabel + "\r\n";
+                    textBox7.Text = textBox7.Text + "  File system: " + d.DriveFormat + "\r\n";
+                    textBox7.Text = textBox7.Text + "  Available space to current user: " + d.AvailableFreeSpace + " bytes" + "\r\n";
+                    textBox7.Text = textBox7.Text + "  Total available space: " + d.TotalFreeSpace + " bytes" + "\r\n";
+                    textBox7.Text = textBox7.Text + "  Total size of drive: " + d.TotalSize + " bytes" + "\r\n";
+                }
+            }
         }
 
         private void Button1_Click(object sender, EventArgs e) {
             FindIP();
-        }
+        }//finds the hostname
 
         private void Form1_Load(object sender, EventArgs e) {
 
-        }
+        }//load
 
         private void FindIP() {
             String returnMessage = String.Empty;
@@ -63,19 +85,29 @@ namespace Lan_Monitor {
 
         private void TextBox5_Click(object sender, EventArgs e) {
             textBox5.Text = "";
-        }
+        }//clears the textbox
 
         private void Form1_Resize(object sender, EventArgs e) {
             if(this.WindowState == FormWindowState.Minimized) {
                 Hide();
                 notifyIcon1.Visible = true;
             }
-        }
+        }//minimize and hide in notification area
 
         private void notifyIcon1_DoubleClick(object sender, EventArgs e) {
             Show();
             this.WindowState = FormWindowState.Normal;
             notifyIcon1.Visible = false;
+        }//maximizes from notification area
+
+        private void button2_Click(object sender, EventArgs e) {
+            SaveFileDialog saveFile = new SaveFileDialog();
+            saveFile.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            saveFile.FilterIndex = 1;
+
+            if (saveFile.ShowDialog() == DialogResult.OK) {
+                File.WriteAllText(saveFile.FileName, textBox6.Text + "\r\n" + textBox7.Text + "\r\n" + textBox5.Text + "\r\n" + textBox3.Text);
+            }
         }
     }
 }
